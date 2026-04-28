ba7aef671248aa84e249c2eac07291de059d0b40
UnderTheBedStudios <162204327+UnderTheBedStudios@users.noreply.github.com>
2026-04-28 18:39:14 -0500

    POWER UP... I only have the single dialog working, nothing else yet.

--- Editor/MainWindow.axaml.cs
+++ Editor/MainWindow.axaml.cs
@@ -1,5 +1,9 @@
-﻿using Avalonia.Controls;
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using LumenX.GameProject;
 
 namespace LumenX;
 
@@ -8,6 +12,24 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        Loaded += OnMainWindowLoaded;
+    }
+
+    private void OnMainWindowLoaded(object sender, RoutedEventArgs e)
+    {
+        Loaded -= OnMainWindowLoaded;
+        OpenProjectBrowserDialog();
+    }
+
+    private async void OpenProjectBrowserDialog()
+    {
+        var dialog = new ProjectBrowserDialog();
+        if (Application.Current?.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+        {
+            await dialog.ShowDialog(desktop.MainWindow);
+
+            desktop.Shutdown();
+        }
     }
 
     // Adding this tells the compiler we expect the XAML to provide the logic
