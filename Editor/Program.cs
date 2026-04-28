3609d407308a04b0983a8c272c64b77faffb4d42
UnderTheBedStudios <162204327+UnderTheBedStudios@users.noreply.github.com>
2026-04-28 17:05:30 -0500

    Init, and yes I'm resetting this whole project because it's not working on Linux, and it was too big to make it work, so I'm making it work.

---
+++ Editor/Program.cs
@@ -0,0 +1,18 @@
+using Avalonia;
+using System;
+
+namespace LumenX;
+
+class Program
+{
+    // Initialization code. Don't use any Visual, System.Drawing, in this method.
+    [STAThread]
+    public static void Main(string[] args) => BuildAvaloniaApp()
+        .StartWithClassicDesktopLifetime(args);
+
+    // Avalonia configuration, don't remove; also used by visual designer.
+    public static AppBuilder BuildAvaloniaApp()
+        => AppBuilder.Configure<App>()
+            .UsePlatformDetect()
+            .LogToTrace();
+}
