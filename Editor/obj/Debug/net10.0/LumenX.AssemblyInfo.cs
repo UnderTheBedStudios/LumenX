c0712a862d812348a53c6cbd21f24cdaa7f1f6fd
UnderTheBedStudios <162204327+UnderTheBedStudios@users.noreply.github.com>
2026-04-29 15:59:23 -0500

    Customizations.

--- Editor/obj/Debug/net10.0/LumenX.AssemblyInfo.cs
+++ Editor/obj/Debug/net10.0/LumenX.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LumenX")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba7aef671248aa84e249c2eac07291de059d0b40")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27d29d6c16767c4befe359b47da4b03a73ab8345")]
 [assembly: System.Reflection.AssemblyProductAttribute("LumenX")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LumenX")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
