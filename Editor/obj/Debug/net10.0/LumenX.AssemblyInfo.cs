eaa8e9b769f1df310f2c1056f7efeb7cc587ad86
UnderTheBedStudios <162204327+UnderTheBedStudios@users.noreply.github.com>
2026-04-30 20:57:04 -0500

    Bug Fixes
    Icons weren't showing up.

--- Editor/obj/Debug/net10.0/LumenX.AssemblyInfo.cs
+++ Editor/obj/Debug/net10.0/LumenX.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LumenX")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87edb634528b70758ce732a185fbe8d7a81ed137")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cee9a9b96ede870c44552d94061bd913fb06024")]
 [assembly: System.Reflection.AssemblyProductAttribute("LumenX")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LumenX")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
