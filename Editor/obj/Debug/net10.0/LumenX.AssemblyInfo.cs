39d89aff44b2fc9b77af6cf0f91139249ec9b08e
UnderTheBedStudios <162204327+UnderTheBedStudios@users.noreply.github.com>
2026-07-11 00:30:46 -0500

    Updating Avalonia

--- Editor/obj/Debug/net10.0/LumenX.AssemblyInfo.cs
+++ Editor/obj/Debug/net10.0/LumenX.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LumenX")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a706ee62f5bd1c3cb03f45d8ffb51892c95fa098")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9b44930ee789b3c1a3a47fa435a5d9142812efc")]
 [assembly: System.Reflection.AssemblyProductAttribute("LumenX")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LumenX")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
