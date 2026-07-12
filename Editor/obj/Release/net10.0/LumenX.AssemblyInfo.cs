569879e1fec362706c3cf150665d649bec4b7f02
UnderTheBedStudios <162204327+UnderTheBedStudios@users.noreply.github.com>
2026-07-12 18:34:36 -0500

    Making a gitignore file :)

--- Editor/obj/Release/net10.0/LumenX.AssemblyInfo.cs
+++ Editor/obj/Release/net10.0/LumenX.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LumenX")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e927e31f4c140e580425451d1e5530ff7a614732")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c009c52617fc1ce721f8e8989390c876e67022ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("LumenX")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LumenX")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
